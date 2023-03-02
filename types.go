a34de19345849e736f8e2f082729bdd21eb29a0b
snail <21599180@qq.com>
2023-03-02 21:32:00 +0800

    bugfix

--- types.go
+++ types.go
@@ -104,7 +104,7 @@ func (rec *Record) AsARecord() (record dns.RR, extras []dns.RR, err error) {
 	if rec.Data == "" {
 		return nil, nil, nil
 	}
-	r.(*dns.A).A = net.ParseIP(rec.Data).To4()
+	r.A = net.ParseIP(rec.Data).To4()
 	fmt.Println(r)
 	fmt.Printf("%#v\n", r.A)
 	return r, nil, nil
